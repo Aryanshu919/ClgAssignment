23bde3ab5206854d6830b0c11d8fa4363a7482e0
Aryanshu919 <92309448+Aryanshu919@users.noreply.github.com>
2022-09-21 20:04:36 +0530

    Conditional Statement.cpp

---
+++ Assignmen-1/Conditional Statement.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main(){
+int no;
+cin >> no;
+if (no ==1) {
+cout << "one";
+}
+else if (no == 2) {
+cout << "two";
+}
+else if (no == 3) {
+cout << "three";
+}
+else if (no == 4) {
+cout << "four";
+}
+else if (no == 5) {
+cout << "five";
+}
+else if (no == 6) {
+cout << "six";
+}
+else if (no == 7){
+    cout << "seven";
+}
+else if (no == 8) {
+cout << "eight";
+}
+else if (no == 9) {
+cout << "nine";
+}
+else  {
+cout << "Greater than 9";
+}
+ 
+ return 0;
+ 
+}
